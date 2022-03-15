7f5e017c8ae2253e3eb1384f6bced3710f399d3a
HasinduLanka <hlrathnayaka@gmail.com>
2022-03-15 20:30:54 +0530

    Bitzquad 404 test

---
+++ _app/chunks/siteUtils-5f48f201.js
@@ -0,0 +1,1 @@
+let l=[".","en","si","fr"],t="en",n=t;function s(e){return e=="."&&(e=t),l.includes(e)?(n=e,n):(n=t,n)}export{s as D,n as l};
---
+++ _app/pages/_pages_/school.svelte-ff6e9bc3.js
@@ -0,0 +1,1 @@
+import{S as e,i as o,s as t}from"../../chunks/vendor-f8e3583d.js";class a extends e{constructor(s){super();o(this,s,null,null,t,{})}}export{a as default};
